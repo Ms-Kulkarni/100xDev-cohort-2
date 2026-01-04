47801552c4dfecec520645dee8cb874a0e52f3ce
M s kulkarni <109647254+Ms-Kulkarni@users.noreply.github.com>
2026-01-04 07:45:50 +0000

    made some changes in authpage

--- Week 22 - excelidraw-main/apps/excelidraw-frontend/components/AuthPage.tsx
+++ Week 22 - excelidraw-main/apps/excelidraw-frontend/components/AuthPage.tsx
@@ -6,14 +6,19 @@ export function AuthPage({isSignin}: {
     return <div className="w-screen h-screen flex justify-center items-center">
         <div className="p-6 m-2 bg-white rounded">
             <div className="p-2">
-                <input type="text" placeholder="Email"></input>
+                <input 
+                type="text"
+                 placeholder="Email"
+                 className="w-full p-2 rounded bg-gray-800 text-white placeholder-gray-400 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-red-400">
+
+                 </input>
             </div>
             <div className="p-2">
                 
             </div>
 
             <div className="pt-2">
-                <button className="bg-red-200 rounded p-2" onClick={() => {
+                <button className="bg-red-200 rounded p-2 w-full" onClick={() => {
 
                 }}>{isSignin ? "Sign in" : "Sign up"}</button>
             </div>
